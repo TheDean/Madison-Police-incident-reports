eb274cc1db9117f445991fee0cf9d9d98f561374
TheDean <madison.comjd@gmail.com>
2016-06-21 14:12:31 -0500

    Initial awesome

---
+++ README.md
@@ -0,0 +1,4 @@
+Madison Police Incident Reports
+================================
+
+This app pulls police incident reports from the City of Madison data website and displays them. The headline links back to the full report. 
---
+++ incidents.py
@@ -0,0 +1,17 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+RSS_FEED = "http://www.cityofmadison.com/police/newsroom/incidentreports/rss.cfm?a=71"
+
+@app.route("/")
+
+def get_incidents():
+	feed = feedparser.parse(RSS_FEED)
+	return render_template("index.html", articles=feed['entries'])
+
+if __name__ == '__main__':
+	app.run(port=5000, debug=True, use_reloader=True)
